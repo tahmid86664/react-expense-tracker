ff77e68338e33a0dfa4e341033860f55cb620c5a
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2022-01-23 11:00:59 +0600

    design updated

--- src/history/History.js
+++ src/history/History.js
@@ -9,14 +9,32 @@ const History = () => {
       </div>
       <div className="history__body">
         <div className="history__item cash__add">
+          <div className="history__delete">
+            <div className="cross">
+              <span className="cross1"></span>
+              <span className="cross2"></span>
+            </div>
+          </div>
           <span>Cash</span>
           <span>+500</span>
         </div>
         <div className="history__item money__expense">
+          <div className="history__delete">
+            <div className="cross">
+              <span className="cross1"></span>
+              <span className="cross2"></span>
+            </div>
+          </div>
           <span>Book</span>
           <span>-40</span>
         </div>
         <div className="history__item money__expense">
+          <div className="history__delete">
+            <div className="cross">
+              <span className="cross1"></span>
+              <span className="cross2"></span>
+            </div>
+          </div>
           <span>Camera</span>
           <span>-200</span>
         </div>
